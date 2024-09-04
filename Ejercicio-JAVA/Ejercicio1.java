8155463fbeb5a4bc39d511508cc0d1ea1bb5b179
Oreana <164940534+YazminAndazabal@users.noreply.github.com>
2024-09-04 12:45:11 -0300

    Create Ejercicio1.java

---
+++ Ejercicio-JAVA/Ejercicio1.java
@@ -0,0 +1,9 @@
+package yaz;
+
+public class yaz {
+	public static void main(String[] args) {
+		int a=5;
+		int b=2;
+		System.out.println("suma " +(a+b)+" resta "+(a-b)+" multiplicacion "+(a*b)+" division "+(a/b)+" modulo "+(a%b));
+	}
+}
