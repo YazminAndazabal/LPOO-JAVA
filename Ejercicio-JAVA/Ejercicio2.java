81a8f909ac0869f71ebea710759e0ff4dfbf8a76
Oreana <164940534+YazminAndazabal@users.noreply.github.com>
2024-09-09 17:48:11 -0300

    Create Ejercicio2.java

---
+++ Ejercicio-JAVA/Ejercicio2.java
@@ -0,0 +1,18 @@
+package yaz;
+
+/*Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los dos. 
+Si son iguales indicarlo también. Ves cambiando los valores para comprobar que funciona.*/
+
+public class Ejercici2{
+	public static void main(String[] args) {
+		int a=5;
+		int b=2;
+		if(a==b)
+			System.out.println("son iguales"); 
+		else if (a>b)
+			System.out.println( a + "ES MAYOR");
+		else if (a<b)
+			System.out.println(b + "es mayor");
+		}
+	}
+	
