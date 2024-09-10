1c8fe3276b12594c78134253a307de31d0528cb0
Oreana <164940534+YazminAndazabal@users.noreply.github.com>
2024-09-10 14:15:26 -0300

    Update Ejercicio1.java

--- Ejercicio-JAVA/Ejercicio1.java
+++ Ejercicio-JAVA/Ejercicio1.java
@@ -1,3 +1,6 @@
+/*Declara dos variables numéricas (con el valor que desees), muestra por consola 
+la suma, resta, multiplicación, división y módulo (resto de la división).*/
+
 package yaz;
 
 public class yaz {
