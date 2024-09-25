71d55fff8d91489de02d7b9c36a3dccf761df74b
Oreana <164940534+YazminAndazabal@users.noreply.github.com>
2024-09-25 12:24:52 -0300

    Create Ejercicio6.java

---
+++ Ejercicio-JAVA/Ejercicio6.java
@@ -0,0 +1,13 @@
+ package yaz;
+/*Lee un número por teclado e indica si es divisible entre 2 (resto = 0). Si no lo es, también debemos indicarlo.*/
+
+import javax.swing.JOptionPane;//libreria para la ventana output
+
+public class yaz {
+	public static void main(String[] args) {
+		String a = JOptionPane.showInputDialog("radio");//ventana con mensaje
+		int radio= Integer.parseInt(a);
+		JOptionPane.showMessageDialog(null, "a"  );
+
+	}
+}
