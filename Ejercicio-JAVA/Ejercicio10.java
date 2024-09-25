29ba9c22b2bdcdde6e02be96e5809c6b51e187e5
Oreana <164940534+YazminAndazabal@users.noreply.github.com>
2024-09-25 12:31:56 -0300

    Create Ejercicio10.java

---
+++ Ejercicio-JAVA/Ejercicio10.java
@@ -0,0 +1,12 @@
+package yaz;
+/*Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.*/
+
+public class yaz {
+	public static void main(String[] args) {
+		int a=1;
+		while(a<=100) {
+			System.out.println(a);
+			a++;
+		}
+	}
+}
