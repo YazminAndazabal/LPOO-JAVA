4112dcb4d265ff1ef7eff65181c692ca45855f30
Oreana <164940534+YazminAndazabal@users.noreply.github.com>
2024-09-25 12:20:45 -0300

    Update Ejercicio4.java

--- Ejercicio-JAVA/Ejercicio4.java
+++ Ejercicio-JAVA/Ejercicio4.java
@@ -1,14 +1,15 @@
 package yaz;
 
+/*Modifica la aplicación anterior, para que nos pida el nombre que queremos introducir (recuerda usar JOptionPane).*/
+
 import javax.swing.JOptionPane;//libreria para la ventana output
 
 public class yaz {
 	public static void main(String[] args) {
 			
-		String a = JOptionPane.showInputDialog("tu nombre papa");//ventana con mensaje a dar
+		String a = JOptionPane.showInputDialog("tu nombre"); //mensaje dado
 			System.out.println("Bienvenido "+a);
-			JOptionPane.showMessageDialog(null, "bienvenido (ahora en ventanita :D) "+a  );
-			//no entiendo exactamente el porque del null
+			JOptionPane.showMessageDialog(null, "bienvenido (ahora en ventana) "+a  );
 
 	}
 }
