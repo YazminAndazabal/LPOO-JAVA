e2e0bffdf174c3e541039c6bb6e7eceb3e333fbf
Oreana <164940534+YazminAndazabal@users.noreply.github.com>
2024-09-26 22:25:21 -0300

    Create Ejercicio19.java

---
+++ Ejercicio-JAVA/Ejercicio19.java
@@ -0,0 +1,18 @@
+package yaz;
+import java.util.Scanner;
+public class yaz19 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int b;
+		do {
+			System.out.println("introduzca numero positivo para contar las cifras");
+			b=sc.nextInt();	
+		}while(b<0);
+		int c=0;
+		for(int a=b;a>0;a/=10) {
+			c++;
+		}
+		System.out.println(c);
+		
+	}
+}
