1e25e786aa065808f8f9680a8628e0fd2d23e452
Oreana <164940534+YazminAndazabal@users.noreply.github.com>
2024-10-02 12:13:17 -0300

    Create Ejercicio23.java

---
+++ Ejercicio-JAVA/Ejercicio23.java
@@ -0,0 +1,7 @@
+package yaz;
+/*Reemplaza todas las a del String anterior por una e.*/
+public class yaz23 {
+	public static void main(String[] args) {
+		  String ea= "la lluvia en sevilla es una maravilla";
+		  ea =ea.replaceAll("a","e");
+		  System.out.println(ea);
